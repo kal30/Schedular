847218721d294480e98f74d82a8ff6b040f6cfd0
Kalyani Rajamani <rkalyani@gmail.com>
2021-01-24 06:52:24 -0600

    PushNotification imported, configured

--- App.js
+++ App.js
@@ -24,6 +24,25 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import PushNotification from 'react-native-push-notification';
+
+PushNotification.configure({
+  onRegister: function (token) {
+    console.log('TOKEN:', token);
+  },
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: Platform.OS === 'ios',
+});
+
 const App: () => React$Node = () => {
   return (
     <>
