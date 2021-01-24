f371072f7121b0194a6a2dcbe18c6a73417dcd47
Kalyani Rajamani <rkalyani@gmail.com>
2021-01-24 12:56:27 -0600

    repeat day commented out, now it does not repeat when the user interacts with it

--- App.js
+++ App.js
@@ -27,72 +27,20 @@ import {
 
 import PushNotification from 'react-native-push-notification';
 
-// PushNotification.configure({
-//   onRegister: function (token) {
-//     console.log('TOKEN:', token);
-//   },
-//   onNotification: function (notification) {
-//     console.log('NOTIFICATION:', notification);
-//     notification.finish(PushNotificationIOS.FetchResult.NoData);
-//   },
-//   permissions: {
-//     alert: true,
-//     badge: true,
-//     sound: true,
-//   },
-//   popInitialNotification: true,
-//   requestPermissions: false,
-//   // requestPermissions: Platform.OS === 'ios',
-// });
-
 // Must be outside of any component LifeCycle (such as `componentDidMount`).
 PushNotification.configure({
-  // (optional) Called when Token is generated (iOS and Android)
-  onRegister: function (token) {
-    console.log('TOKEN:', token);
-  },
-
   // (required) Called when a remote is received or opened, or local notification is opened
   onNotification: function (notification) {
-    console.log('NOTIFICATION:', notification);
-
-    // process the notification
-
-    // (required) Called when a remote is received or opened, or local notification is opened
-    // notification.finish(PushNotificationIOS.FetchResult.NoData);
+    console.log('onNotification:', notification);
+    notification.finish(true);
   },
-
-  // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-  onAction: function (notification) {
-    console.log('ACTION:', notification.action);
-    console.log('NOTIFICATION:', notification);
-
-    // process the action
-  },
-
-  // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-  onRegistrationError: function (err) {
-    console.error(err.message, err);
-  },
-
   // IOS ONLY (optional): default: all - Permissions to register.
   permissions: {
     alert: true,
     badge: true,
     sound: true,
   },
-
-  // Should the initial notification be popped automatically
-  // default: true
   popInitialNotification: true,
-
-  /**
-   * (optional) default: true
-   * - Specified if permissions (ios) and token (android and ios) will requested or not,
-   * - if not, you must call PushNotificationsHandler.requestPermissions() later
-   * - if you are not using remote notification or do not have Firebase installed, use this:
-   *     requestPermissions: Platform.OS === 'ios'
-   */
   requestPermissions: false,
 });
 
@@ -113,45 +61,26 @@ const App: () => React$Node = () => {
 
   function testPush() {
     console.log(new Date() + 'invoking function testPush');
-    // if (!PushNotification.channelExists('fooChannel')) {
-    //   PushNotification.createChannel(
-    //     {
-    //       channelId: 'fooChannel', // (required)
-    //       channelName: 'My channel', // (required)
-    //       channelDescription: 'A channel to categorise your notifications', // (optional) default: undefined.
-    //       playSound: true, // (optional) default: true
-    //       soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
-    //       importance: 4, // (optional) default: 4. Int value of the Android notification importance
-    //       vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
-    //     },
-    //     (created) => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
-    //   );
-    // }
-    // PushNotification.getChannels(function (channel_ids) {
-    //   console.log(channel_ids);
-    // });
     PushNotification.localNotification({
       channelId: 'fooChannel', // (required) channelId, if the channel doesn't exist, it will be created with options passed above (importance, vibration, sound). Once the channel is created, the channel will not be update. Make sure your channelId is different if you change these options. If you have created a custom channel, it will apply options of the channel.
-      bigText: 'My big text that will be shown when notification is expanded', // (optional) default: "message" prop
-      subText: 'This is a subText', // (optional) default: none
-      ongoing: true, // (optional) set whether this is an "ongoing" notification
-      visibility: 'public', // (optional) set notification visibility, default: private
-      ignoreInForeground: false, // (optional) if true, the notification will not be visible when the app is in the foreground (useful for parity with how iOS notifications appear). should be used in combine with `com.dieam.reactnativepushnotification.notification_foreground` setting
-      shortcutId: 'shortcut-id', // (optional) If this notification is duplicative of a Launcher shortcut, sets the id of the shortcut, in case the Launcher wants to hide the shortcut, default undefined
-      onlyAlertOnce: true, // (optional) alert will open only once with sound and notify, default: false
-      when: new Date(), // (optionnal) Add a timestamp pertaining to the notification (usually the time the event occurred). For apps targeting Build.VERSION_CODES.N and above, this time is not shown anymore by default and must be opted into by using `showWhen`, default: null.
-      actions: ['Yes', 'No'], // (Android only) See the doc for notification actions to know more
-      /* iOS only properties */
-      category: '', // (optional) default: empty string
+      // ongoing: true, // (optional) set whether this is an "ongoing" notification
+      // visibility: 'public', // (optional) set notification visibility, default: private
+      // ignoreInForeground: false, // (optional) if true, the notification will not be visible when the app is in the foreground (useful for parity with how iOS notifications appear). should be used in combine with `com.dieam.reactnativepushnotification.notification_foreground` setting
+      // shortcutId: 'shortcut-id', // (optional) If this notification is duplicative of a Launcher shortcut, sets the id of the shortcut, in case the Launcher wants to hide the shortcut, default undefined
+      // onlyAlertOnce: true, // (optional) alert will open only once with sound and notify, default: false
+      // when: new Date(), // (optionnal) Add a timestamp pertaining to the notification (usually the time the event occurred). For apps targeting Build.VERSION_CODES.N and above, this time is not shown anymore by default and must be opted into by using `showWhen`, default: null.
+      // actions: ['Yes', 'No'], // (Android only) See the doc for notification actions to know more
+      // /* iOS only properties */
+      // category: '', // (optional) default: empty string
 
       /* iOS and Android properties */
       // id: 0, // (optional) Valid unique 32 bit integer specified as string. default: Autogenerated Unique ID
       title: 'My Notification Title', // (optional)
       message: 'My Notification Message', // (required)
-      userInfo: {}, // (optional) default: {} (using null throws a JSON value '<null>' error)
+      // userInfo: {}, // (optional) default: {} (using null throws a JSON value '<null>' error)
       playSound: true, // (optional) default: true
       soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
-      repeatType: 'day', // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
+      // repeatType: 'day', // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
     });
   }
 
